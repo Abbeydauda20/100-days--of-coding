459517975dd2f101d2df1fc36d685fe4173b747b
abbeydauda20 <33432297-abbeydauda20@users.noreply.replit.com>
2024-04-14 22:30:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+#print("Hello world")
+age = int(input("Enter your age: "))
+if age >= 18:
+  print(f"You are {age} years old, you can vote")
+else:
+  print(f"You are {age} years old, you can't vote")
